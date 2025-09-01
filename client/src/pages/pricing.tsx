bbb4e1077c934a685921eeed17e662e4a1f1edf5
hydrademonexe <kauanpimenteljob@gmail.com>
2025-09-01 12:56:40 -0300

    Fix JSX structure in pricing component

--- client/src/pages/pricing.tsx
+++ client/src/pages/pricing.tsx
@@ -214,7 +214,6 @@ export default function Pricing() {
                   </div>
                 )}
               </div>
-              </div>
 
               {/* Results */}
               <Card className="bg-muted">
